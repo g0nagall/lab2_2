20a6f6302c5cc937872f4384f442f2750623de46
g0nagall <50554188+g0nagall@users.noreply.github.com>
2019-05-28 02:52:01 +0300

    Add files via upload

---
+++ lab2_2/lab2_2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2_2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Type x");
+			double x = double.Parse(Console.ReadLine());
+			double a = 3, c = 5, b = 4, d = 6;
+			double result1 = Math.Tan(x / 7 * +a) - Math.Log(Math.Abs(b * 7 + 7));
+			double result = c * (Math.Pow(((x * x) + Math.Pow(7, 1.2) * d), 1 / 7));
+			if (Math.Abs(x) >= 10) Console.WriteLine("y = " + result);
+			else Console.WriteLine("y = " + result1);
+		}
+	}
+}
+
